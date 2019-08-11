38705ae43afd8a560779b42fb745afbbd7b05894
Lzhm <443964837@qq.com>
2019-08-12 01:15:28 +0800

    userdetail test3

--- pkg/dingtalk/h5.go
+++ pkg/dingtalk/h5.go
@@ -3,6 +3,7 @@ package dingtalk
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/goinggo/mapstructure"
 	"github.com/parnurzeal/gorequest"
 	"github.com/selinplus/go-dingtalk/models"
 	"github.com/selinplus/go-dingtalk/pkg/logging"
@@ -259,16 +260,16 @@ func DepartmentUserDetail(id, pageNum int) []*models.User {
 			log.Printf("unmarshall userlist info error:%v", err)
 		}
 		if userlist["userlist"] != nil {
-			log.Println("userlist[\"userlist\"] is %v", userlist["userlist"])
 			users := userlist["userlist"].([]interface{})
-			log.Println("userlist[\"userlist\"].([]interface{}) is %v", users)
 			for _, v := range users {
 				vv := v.(map[string]interface{})
 				log.Println("vv is %v", vv)
+				if err := mapstructure.Decode(vv, &user); err != nil {
+					log.Printf("convert struct error:%v", err)
+				}
+				user.SyncTime = time.Now().Format("2006-01-02 15:04:05")
 				for k, val := range vv {
-					switch k {
-					case "department":
-						log.Println("val is %v", val)
+					if k == "department" {
 						var paramSlice []string
 						for _, d := range val.([]interface{}) {
 							v := strconv.Itoa(int(d.(float64)))
@@ -276,32 +277,8 @@ func DepartmentUserDetail(id, pageNum int) []*models.User {
 						}
 						depIds := strings.Join(paramSlice, ",")
 						user.Department = depIds
-					case "userid":
-						user.UserID = val.(string)
-					case "mobile":
-						user.Name = val.(string)
-					case "isAdmin":
-						user.IsAdmin = val.(bool)
-					case "active":
-						user.Active = val.(bool)
-					case "avatar":
-						user.Avatar = val.(string)
-					case "remark":
-						user.Remark = val.(string)
-					default:
-						log.Println("error")
 					}
-					//	if k == "department" {
-					//		var paramSlice []string
-					//		for _, d := range val.([]interface{}) {
-					//			v := strconv.Itoa(int(d.(float64)))
-					//			paramSlice = append(paramSlice, v)
-					//		}
-					//		depIds := strings.Join(paramSlice, ",")
-					//		user.Department = depIds
-					//	}
 				}
-				user.SyncTime = time.Now().Format("2006-01-02 15:04:05")
 				log.Printf("user is:%v", user)
 			}
 		}
