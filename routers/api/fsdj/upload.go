ccdee758e980d822264797f76a2e249ecce175bd
Lzhm <443964837@qq.com>
2020-12-09 10:29:03 +0800

    change fsdj/upload return url

--- routers/api/fsdj/upload.go
+++ routers/api/fsdj/upload.go
@@ -1,7 +1,6 @@
 package fsdj
 
 import (
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/selinplus/go-dingtalk/pkg/app"
 	"github.com/selinplus/go-dingtalk/pkg/e"
@@ -47,22 +46,17 @@ func FsdjUploadFile(c *gin.Context) {
 		return
 	}
 
-	session := sessions.Default(c)
 	var url string
-	if session.Get("userid") != nil { //if H5 return internetIP/api/v2/...
-		url = upload.GetAppImageFullUrl(imageName)
-	} else {
-		token := c.GetHeader("Authorization")
-		auth := c.Query("token")
-		if len(auth) > 0 {
-			token = auth
-		}
-		ts := strings.Split(token, ".")
-		if len(ts) == 4 { //if token.length==4 then eapp, return internetIP/api/fsdj/...
-			url = fsdjsrv.GetFsdjEappImageFullUrl(imageName)
-		} else { //inner  return innerIP/...
-			url = fsdjsrv.GetFsdjImageFullUrl(imageName)
-		}
+	token := c.GetHeader("Authorization")
+	auth := c.Query("token")
+	if len(auth) > 0 {
+		token = auth
+	}
+	ts := strings.Split(token, ".")
+	if len(ts) == 4 { //if token.length==4 then eapp, return internetIP/api/fsdj/...
+		url = fsdjsrv.GetFsdjEappImageFullUrl(imageName)
+	} else { //inner  return innerIP/...
+		url = fsdjsrv.GetFsdjImageFullUrl(imageName)
 	}
 
 	appG.Response(http.StatusOK, e.SUCCESS, map[string]string{
